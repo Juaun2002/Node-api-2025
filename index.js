ae43eb125636f3211b10b164ed1c96a0683d895f
Juaun2002 <jvpessoa113@gmail.com>
2025-06-02 09:05:45 -0300

    Começando a entender sobre parametros

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 
+app.get("/",function(req,res){
+    res.send("Ola mundo")
+});
+
+app.get("/sobre",function(req,res){
+    res.send("Minha pagina do cassete")
+});
+
+app.get("/contato",function(req,res){
+    res.send("Minha pagina de contato")
+});
+
+app.get("/ola/:nome/:sobrenome/:cor",function(req,res){
+    res.send("<h1>Ola " + req.params.nome + " " + req.params.sobrenome + req.params.cor +"</h1>");
+});
+
+
+
+
 
 app.listen(8081,function(){
     console.log("O servidor esta rodando na url http://localhost:8081")
